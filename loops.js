b5b127e234cd16a2c3b886a38309171cbb6d9fd8
basvanderkamp <basvanderkamp@hotmail.nl>
2022-10-12 11:30:18 +0200

    huiswerk gemaakt

--- loops.js
+++ loops.js
@@ -5,6 +5,10 @@
 // Hoera!
 // Hoera!
 // ==========================================
+for (let i = 0; i < 3; i++) {
+    console.log("Hoera!");
+
+}
 
 
 
@@ -17,9 +21,10 @@
 // loop...
 // klaar!
  // ==========================================
-
-
-
+for (let i = 0; i < 4; i++) {
+    console.log("loop...");
+}
+console.log("klaar!");
 // ==========================================
 // Opdracht 3. Maak een for-loop die van 0 tot 15 loopt. Voor ieder getal wordt in de console gelogd of het getal even of oneven is.
 // Verwachte uitkomsten:
@@ -29,6 +34,16 @@
 // 3 is oneven
 // .... etc.
 // 15 is oneven
+
+for (let i = 0; i < 16; i++) {
+
+    if (i%2===0) {
+        console.log(i, "is even");
+    } else {
+        console.log(i, "is oneven");
+    }
+
+}
 
 // Tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door 2 en er blijft niets over. Google daarom eens naar 'mdn webdocs remainder operator'.
 // ==========================================
@@ -51,3 +66,16 @@
 // >> 9
 // ==========================================
 
+for (let i = 0; i < 10; i++) {
+
+    if (i<4) {
+        console.log(i);
+    } else if (i<7) {
+        console.log(">"+i);
+    } else {
+        console.log(">>"+i);
+    }
+
+
+
+}
